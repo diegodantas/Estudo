9b54592e3b7acedc05a3b9e4a39a25f7e5cf8b7e
DIEGO <DIEGO@DIEGO-PC>
2015-08-29 13:48:08 -0300

    Nova annotation

---
+++ src/main/java/br/com/caelum/vraptor/annotations/Public.java
@@ -0,0 +1,15 @@
+package br.com.caelum.vraptor.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/*Criando um nova Annotation*/
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Public {
+
+}
